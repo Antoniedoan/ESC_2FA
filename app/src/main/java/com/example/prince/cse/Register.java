72f7e2f30d952e0aaacd93bbc7efa2594e4bd8f1
Thanh <antoniedoan@gmail.com>
2018-04-03 19:30:20 +0800

    updated Register with OTP

--- app/src/main/java/com/example/prince/cse/Register.java
+++ app/src/main/java/com/example/prince/cse/Register.java
@@ -1,13 +1,36 @@
 package com.example.prince.cse;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.SecureRandom;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import static android.Manifest.permission.SEND_SMS;
 
 public class Register extends AppCompatActivity {
+
 
     private BackgroundTask backgroundTask = null;
 
@@ -16,7 +39,11 @@ public class Register extends AppCompatActivity {
     EditText inputPassword;
     EditText inputConfirmPassword;
 
+
     Button buttonRegister;
+
+
+    public static String pin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +57,19 @@ public class Register extends AppCompatActivity {
 
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
+
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                attemptRegister();
+                attemptSendEmail();
 
             }
         });
 
     }
 
-    private void attemptRegister() {
+    private void attemptSendEmail() {
         if (backgroundTask != null) {
             return;
         }
@@ -62,15 +90,12 @@ public class Register extends AppCompatActivity {
         View focusView = null;
 
 
-
         // Check for a valid full name.
         if (TextUtils.isEmpty(name)) {
             inputName.setError(getString(R.string.error_field_required));
             focusView = inputName;
             cancel = true;
         }
-
-
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
@@ -116,23 +141,31 @@ public class Register extends AppCompatActivity {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
 
+            //sendEmail();
 
 
-            name = inputName.getText().toString();
-            email = inputEmail.getText().toString();
-            password = inputPassword.getText().toString();
-            String method = "register";
-            backgroundTask = new BackgroundTask(getApplicationContext());
-            backgroundTask.execute(method,name,email,password);
+            Intent intent = new Intent(getApplicationContext(), OTPVerification.class);
+            intent.putExtra("name",inputName.getText().toString());
+            intent.putExtra("password",inputPassword.getText().toString());
+            intent.putExtra("email",inputEmail.getText().toString());
+            startActivity(intent);
 
-            finish();
 
         }
     }
 
+
+
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
     }
 
     private boolean isPasswordValid(String password) {
@@ -141,9 +174,90 @@ public class Register extends AppCompatActivity {
     }
 
 
+    private static String generateOTP() {
 
+        String chars = "abcdefghijklmnopqrstuvwxyz"
+                + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789!@%$%&^?|~'\"#+="
+                + "\\*/.,:;[]()-_<>";
 
+        final int PW_LENGTH = 6;
+        Random rnd = new SecureRandom();
+        StringBuilder pass = new StringBuilder();
+        for (int i = 0; i < PW_LENGTH; i++)
+            pass.append(chars.charAt(rnd.nextInt(chars.length())));
 
+        pin = pass.toString();
+        return pin;
+    }
 
+    private void sendEmail(){
+//        final String to = inputEmail.getText().toString();
+//        final String from = "antoniedoan@gmail.com";
+//        final String pw = "1234abcd!@#$";
+//
+//
+//        Properties properties = System.getProperties();
+//        properties.put("mail.smtp.starttls.enable", "true");
+//        properties.put("mail.smtp.auth", "true");
+//        properties.put("mail.smtp.host", "smtp.gmail.com");
+//        properties.put("mail.smtp.port", "587");
+//
+//        Session session = Session.getInstance(properties, new Authenticator() {
+//            @Override
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication(to, pw);
+//            }
+//        });
+//
+//        try{
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress(from));
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//
+//            message.setSubject("Verification code for account activation");
+//            message.setText("Please key in this code to activate your account: " + generateOTP());
+//            Transport.send(message);
+//            System.out.println("Message sent successfully");
+//
+//        } catch (MessagingException e){
+//            throw new RuntimeException(e);
+//        }
+
+        String[] to = {"esc.kyc1@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "OTP VERIFICATION");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Please enter this OTP: "+generateOTP());
+        try{
+            startActivity(Intent.createChooser(emailIntent, "Sending email..."));
+            //setContentView(R.layout.activity_verify_email);
+            Log.i("Email sent successfully", "");
+        } catch (ActivityNotFoundException ex){
+
+        }
+
+//        try {
+//            String phoneNo = "6586178715";
+//
+//            SmsManager sms = SmsManager.getDefault();
+//            sms.sendTextMessage(phoneNo, null, generateOTP(), null, null);
+//            Toast.makeText(this, "Sent", Toast.LENGTH_SHORT).show();
+//
+////            Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+////            sendIntent.putExtra("sms_body", generateOTP());
+////            sendIntent.setType("vnd.android-dir/mms-sms");
+////            startActivity(sendIntent);
+//
+//        } catch (Exception e) {
+//            Toast.makeText(getApplicationContext(),
+//                    "SMS failed, please try again later!",
+//                    Toast.LENGTH_LONG).show();
+//            e.printStackTrace();
+//        }
+
+    }
 
 }
